f2643397a5030fddb88b4e7a6e509ce473f8ee3e
Luke Palmer <lrpalmer@gmail.com>
2009-06-10 17:44:03 -0600

    Forgot to commit this file.

---
+++ experiments/interp-stack/InterpStack/Exp.hs
@@ -0,0 +1,16 @@
+module InterpStack.Exp where
+
+infixl 9 :%
+data Exp a
+    = Exp a :% Exp a
+    | Lam (Exp a)
+    | Var Int
+    | Lit a
+    deriving (Show)
+
+class Value v where
+    applyValue :: v -> v -> v
+
+data Interp = Interp {
+    eval :: forall v. (Value v) => Exp v -> Maybe v
+}
